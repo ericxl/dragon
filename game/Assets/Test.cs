011c9918589657abd284a503bb67aba81103df27
Eric Liang <jklelxy@gmail.com>
2017-03-01 23:17:15 -0600

    project setup

---
+++ game/Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
