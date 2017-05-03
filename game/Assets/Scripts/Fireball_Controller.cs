18dccc511c8287b007b8a78c2dc60d7034694073
zhang1995 <zhangtong@utexas.edu>
2017-05-02 19:05:12 -0500

    the attack function is done, next I will add player health

---
+++ game/Assets/Scripts/Fireball_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Fireball_Controller : NetworkBehaviour {
+	[SerializeField] float lifetime = 4f;
+	[SerializeField] bool cankill = false;
+
+
+	bool isLive = true;
+	float age;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	//[ServerCallback]
+	void FixedUpdate () {
+		
+		age += Time.deltaTime;
+		if (age > lifetime) {
+			NetworkServer.Destroy (gameObject);
+		} else {
+
+			transform.Translate (Vector2.right * Time.deltaTime*8);
+		}
+	}
+	[Command]
+	void CmdMove() {
+		
+		transform.Translate (Vector2.right * Time.deltaTime*8);
+	}
+	void OnCollisionEnter(Collision other) {
+		if (!isLive)
+			return;
+		isLive = false;
+
+
+		if (!isServer)
+			return;
+
+		if (!cankill || other.gameObject.tag != "Player")
+			return;
+
+		Debug.Log ("you hit a player");
+	}
+}
